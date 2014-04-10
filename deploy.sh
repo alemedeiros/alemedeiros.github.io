37e7a3476db14d8bfa3876ee0d7f601f946ebd72
Alexandre Medeiros <alexandre.n.medeiros@gmail.com>
2014-04-10 00:53:09 -0300

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+./homepage build
+
+case $1 in
+  ic)
+    rsync -az _site/* ic:~/public_html
+    ;;
+  sdf|*)
+    tar cjf hpg.tar.bz2 -C _site/ .
+    echo "put hpg.tar.bz2" | sftp sdf.org
+    ssh sdf.org "tar xjf hpg.tar.bz2 -C html; mkhomepg -p; rm hpg.tar.bz2"
+    rm hpg.tar.bz2
+    ;;
+esac
