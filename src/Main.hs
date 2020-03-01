2e6df2994c9ccb9439112c40f3cdc69c0a3520d3
Alexandre Medeiros <alexandre.medeiros@nubank.com.br>
2020-03-01 15:24:22 -0300

    Reviving the blog code!

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,19 @@
 --------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
+
+import           Data.List                       (intersperse)
+import           Data.Monoid                     (mappend)
+import           Text.Blaze.Html                 (toHtml, toValue, (!))
+import qualified Text.Blaze.Html5                as H
+import qualified Text.Blaze.Html5.Attributes     as A
+
 import           Hakyll
 
 --------------------------------------------------------------------------------
 main :: IO ()
 main = hakyll $ do
     -- Images
-    -- TODO(alemedeiros): compress images before deploying / try to make this automatic
+    -- TODO: compress images before deploying / try to make this automatic
     match ("images/*" .||. "favicon.ico") $ do
         route   idRoute
         compile copyFileCompiler
@@ -30,18 +36,18 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/base.html"    defaultContext
             >>= relativizeUrls
 
-    -- TODO(alemedeiros): Teaching pages
+    -- TODO: Teaching pages
 
-    -- TODO(alemedeiros): Create Categories pages
+    -- TODO: Create Categories pages
     tags <- buildTags "posts/*" (fromCapture "tags/*.html")
     tagsRules tags $ \tag pattern -> do
         route idRoute
         compile $ do
             posts <- recentFirst =<< loadAll pattern
             let tagCtx =
-                    constField "title" (tag ++ " Posts")                    `mappend`
-                    constField "tab_blog" ""                                `mappend`
-                    listField "posts" (postCtxWithTags tags) (return posts) `mappend`
+                    constField "title" (tag ++ " Posts")                        `mappend`
+                    constField "tab_blog" ""                                    `mappend`
+                    listField "posts" (postCtxWithChipTags tags) (return posts) `mappend`
                     defaultContext
 
             makeItem ""
@@ -54,20 +60,20 @@ main = hakyll $ do
         route $ setExtension "html"
         compile $ pandocCompiler
             >>= saveSnapshot "content"
-            >>= loadAndApplyTemplate "templates/content.html" (postCtxWithTags tags)
-            >>= loadAndApplyTemplate "templates/base.html"    (postCtxWithTags tags)
+            >>= loadAndApplyTemplate "templates/content.html" (postCtxWithChipTags tags)
+            >>= loadAndApplyTemplate "templates/base.html"    (postCtxWithChipTags tags)
             >>= relativizeUrls
 
     -- Blog main page
-    -- TODO(alemedeiros): Paginate blog page
+    -- TODO: Paginate blog page
     create ["blog.html"] $ do
         route idRoute
         compile $ do
             posts <- recentFirst =<< loadAllSnapshots "posts/*" "content"
             let archiveCtx =
-                    constField "tab_blog" ""                                `mappend`
-                    listField "posts" (postCtxWithTags tags) (return posts) `mappend`
-                    constField "title" "Blog posts"                         `mappend`
+                    constField "tab_blog" ""                                    `mappend`
+                    listField "posts" (postCtxWithChipTags tags) (return posts) `mappend`
+                    constField "title" "Blog posts"                             `mappend`
                     defaultContext
 
             makeItem ""
@@ -84,7 +90,7 @@ main = hakyll $ do
             let indexCtx =
                     constField "tab_home" ""                                `mappend`
                     constField "card" ""                                    `mappend`
-                    listField "posts" (postCtxWithTags tags) (return posts) `mappend`
+                    listField "posts" (postCtxWithChipTags tags) (return posts) `mappend`
                     defaultContext
 
             getResourceBody
@@ -101,8 +107,17 @@ postCtx :: Context String
 postCtx =
     teaserField "teaser" "content" `mappend`
     constField "tab_blog" "" `mappend`
-    dateField "date" "%B %e, %Y" `mappend`
+    dateField "date" "%e %B, %Y" `mappend`
     defaultContext
 
 postCtxWithTags :: Tags -> Context String
 postCtxWithTags tags = tagsField "tags" tags `mappend` postCtx
+
+postCtxWithChipTags :: Tags -> Context String
+postCtxWithChipTags tags = tagsFieldWith getTags renderTagWithChip (mconcat . intersperse " ") "chiptags" tags `mappend` (postCtxWithTags tags)
+  where
+    renderTagWithChip _ Nothing = Nothing
+    renderTagWithChip tag (Just filePath) =
+      Just $ H.a ! A.href (toValue $ toUrl filePath) $ mdlChip $ toHtml tag
+
+    mdlChip = (H.span ! A.class_ "mdl-chip") . (H.span ! A.class_ "mdl-chip__text")
